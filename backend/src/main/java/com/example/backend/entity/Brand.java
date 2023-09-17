40a5ed951c4574bb210f4fdbc02f887e7c681b52
Ravshan8860 <127499325+Ravshan88@users.noreply.github.com>
2023-09-17 20:01:44 +0500

    first change

---
+++ backend/src/main/java/com/example/backend/entity/Brand.java
@@ -0,0 +1,24 @@
+package com.example.backend.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Brand {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+    private String name;
+    @OneToMany
+    private List<Attachment> photo;
+}
---
+++ backend/src/main/java/com/example/backend/repository/RoleRepository.java
@@ -0,0 +1,9 @@
+package com.example.backend.repository;
+
+import com.example.backend.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface RoleRepository extends JpaRepository<Role, UUID> {
+}
